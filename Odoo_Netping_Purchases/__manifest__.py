6db848c9a26f772fb5290d1391720a8049778c84
Netping <sp@netping.ru>
2022-07-16 12:06:23 +0800

    Add files via upload

---
+++ Odoo_Netping_Purchases/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Odoo_Netping_Purchases',
+    'version' : '1.1',
+    'summary': 'Purchase',
+    'author': 'Andrei Zelenin',
+    'sequence': 0,
+    'description': """
+
+The specific module for customize standart Purchase module for Netping.
+
+    """,
+    'category': 'Inventory/Purchase',
+    'depends' : ['purchase'],
+    'data': [
+        'views/purchase_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
