9292d9c5ccf6d6cff99959e508bb9bd70f86eb7e
Andrei Zelenin <andrei_zelenin@inbox.ru>
2023-04-05 01:10:52 +0300

    Task https://netping.teamwork.com/app/tasks/36498013?c=17394182 has done

--- Odoo_Netping_Inventory/__manifest__.py
+++ Odoo_Netping_Inventory/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'Odoo_Netping_Inventory',
-    'version' : '1.1',
+    'version' : '1.2',
     'summary': 'Manage your stock and logistics activities',
     'author': 'Andrei Zelenin',
     'sequence': 0,
@@ -13,7 +13,13 @@ The specific module for customize standart Inventory module for Netping.
     'category': 'Inventory',
     'depends' : ['stock'],
     'data': [
+	    'views/product_views.xml',
+		'views/stock_quant_views.xml',
         'views/stock_picking_views.xml',
+		'views/stock_menu_views.xml',
+		'views/stock_orderpoint_views.xml',
+		'views/stock_scrap_views.xml',
+		'wizard/stock_scheduler_compute_views.xml',
     ],
     'installable': True,
     'application': True,
