895a56613587e465ded7981434504ea539c9e765
Andrei Zelenin <andrei_zelenin@inbox.ru>
2023-04-06 20:31:11 +0300

    override Product Move default search view

--- Odoo_Netping_Inventory/__manifest__.py
+++ Odoo_Netping_Inventory/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'Odoo_Netping_Inventory',
-    'version' : '1.2',
+    'version' : '1.3',
     'summary': 'Manage your stock and logistics activities',
     'author': 'Andrei Zelenin',
     'sequence': 0,
@@ -19,6 +19,7 @@ The specific module for customize standart Inventory module for Netping.
 		'views/stock_menu_views.xml',
 		'views/stock_orderpoint_views.xml',
 		'views/stock_scrap_views.xml',
+		'views/stock_move_line_views.xml',
 		'wizard/stock_scheduler_compute_views.xml',
     ],
     'installable': True,
