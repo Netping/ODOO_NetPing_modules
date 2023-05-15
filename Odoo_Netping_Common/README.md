2ca37bede7fc74325f9c2cfc3a0d718db27fa1b9
Andrei Zelenin <andrei_zelenin@inbox.ru>
2023-05-15 23:08:17 +0300

    Initial Commit

---
+++ Odoo_Netping_Common/README.md
@@ -0,0 +1,17 @@
+Odoo_Netping_Common
+https://netping.atlassian.net/wiki/
+------------------------
+
+# -- Install --
+1) Put sourse cod of Netping Common module to the addons path
+2) Switch on developer mode
+3) Update app list
+4) DELETE "report.url" System Parameter from Menu "Settings --> Technical --> Parameters --> System Parameters"
+5) Search and click to install Netping Common module
+6) Set "East A4 portrait" Paperformat and "external_layout_netping" Layout in "Business Documents" part of Settings Menu
+
+
+
+
+
+
---
+++ Odoo_Netping_Common/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Odoo_Netping_Common',
+    'version' : '1.0',
+    'summary': 'Common',
+    'author': 'Andrei Zelenin',
+    'sequence': 0,
+    'description': """
+
+The specific module for general improvements for Netping.
+
+    """,
+    'category': 'Extra Tools',
+    'depends' : ['mail'],
+    'data': [
+		'security/common_groups.xml',
+        'views/common_menus.xml',
+		'report/common_report_templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
