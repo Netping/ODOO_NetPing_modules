aeb8ec625fa9ec8f7dee6fe3603ff250b98dc200
Andrei Zelenin <andrei_zelenin@inbox.ru>
2023-10-05 09:21:03 +0300

    [src] task https://netping.teamwork.com/app/tasks/38116355 PART_1

--- Odoo_Netping_Sales/__manifest__.py
+++ Odoo_Netping_Sales/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'Odoo_Netping_Sales',
-    'version' : '3.2',
+    'version' : '3.3',
     'summary': 'Sales',
     'author': 'Andrei Zelenin',
     'sequence': 0,
@@ -11,7 +11,7 @@ The specific module for customize standart Sales module for Netping.
 
     """,
     'category': 'Sales',
-    'depends' : ['sale'],
+    'depends' : ['sale', 'Odoo_Netping_Common', 'Odoo_Netping_Inventory'],
     'data': [
         'views/sale_views.xml',
         'report/sale_report.xml',
