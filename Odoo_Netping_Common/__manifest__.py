962ac47ed80c7a32bda4c5d71b617f507f59037b
NetpingSoft <147425850+NetpingSoft@users.noreply.github.com>
2024-01-11 10:54:22 +0400

    Bump version

--- Odoo_Netping_Common/__manifest__.py
+++ Odoo_Netping_Common/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'Odoo_Netping_Common',
-    'version' : '1.4',
+    'version' : '1.5',
     'summary': 'Common',
     'author': 'Andrei Zelenin',
     'sequence': 0,
