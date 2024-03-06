50d25a185700ff147fb295ac619ed2949899dc63
Andrei Zelenin <andrei_zelenin@inbox.ru>
2024-03-06 11:15:40 +0300

    [src] Task https://netping.teamwork.com/app/tasks/39543276

--- Odoo_Netping_Sales/__manifest__.py
+++ Odoo_Netping_Sales/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name' : 'Odoo_Netping_Sales',
-    'version' : '4.4',
+    'version' : '4.5',
     'summary': 'Sales',
     'author': 'Andrei Zelenin',
     'sequence': 0,
